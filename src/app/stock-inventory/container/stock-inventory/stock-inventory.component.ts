18d426a75c0641c480cfc58aea975b3aef52dbb6
sheeba <developer.sheeba@gmail.com>
2022-01-12 12:36:22 +0530

    FormControls and FormGroups

--- src/app/stock-inventory/container/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/container/stock-inventory/stock-inventory.component.ts
@@ -1,16 +1,47 @@
-import { Template } from "@angular/compiler/src/render3/r3_ast";
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormArray } from "@angular/forms";
+
  
 @Component({
     selector: 'stock-inventory',
     styleUrls: ['stock-inventory.component.scss'],
     template: `
         <div class="stock-inventory">
-            Hello world
+            <form [formGroup]="form" (ngSubmit)="onSubmit()">
+                <div formGroupName="store">
+                    <input 
+                        type="text" 
+                        placeholder="Branch ID"
+                        formControlName="branch">
+                    <input 
+                        type="text" 
+                        placeholder="Manager Code"
+                        formControlName="code">
+                </div>
+
+                <div class="stock-inventory__buttons">
+                    <button 
+                        type="submit"
+                        [disabled]="form.invalid">
+                        Order stock
+                    </button>
+                </div>
+
+                <pre>{{form.value | json}}</pre>
+            </form>
         </div>
     `
 })
 
 export class StockInventoryComponent {
+    form = new FormGroup({
+        store: new FormGroup({
+            branch: new FormControl('B182'),
+            code: new FormControl('1234')
+        })
+    })
 
+    onSubmit() {
+        console.log(this.form.value)
+    }
 }
