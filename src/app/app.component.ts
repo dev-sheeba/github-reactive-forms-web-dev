192e006561e932865dfd06f9436c4b5ade704e1d
sheeba <developer.sheeba@gmail.com>
2022-01-12 11:01:53 +0530

    Reactive form setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <stock-inventory></stock-inventory>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  
 }
---
+++ src/app/stock-inventory/container/stock-inventory/stock-inventory.component.ts
@@ -0,0 +1,16 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+ 
+@Component({
+    selector: 'stock-inventory',
+    styleUrls: ['stock-inventory.component.scss'],
+    template: `
+        <div class="stock-inventory">
+            Hello world
+        </div>
+    `
+})
+
+export class StockInventoryComponent {
+
+}
